08b3eee10cd07a0e8694bf93a05bff1be1bde9c5
Takafumi Suzuki <tk4fm1@gmail.com>
2020-07-04 23:54:54 +0900

    SLIM並列化

--- SLIM_test/SLIM_model.py
+++ SLIM_test/SLIM_model.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 from scipy.sparse import csr_matrix
 from sklearn.linear_model import ElasticNet, Ridge, Lasso
+from multiprocessing import Pool
+import multiprocessing as multi
+from joblib import Parallel, delayed
 
 
 class SLIM():
@@ -29,19 +32,35 @@ class SLIM():
             X = self.del_col(i)
             y = self.rating_mat[:, i]
     
-            #reg = ElasticNet(alpha=0.0001, positive=True)
-            #reg = Ridge()
-            #reg = Lasso(alpha=0.0001, positive=True)
             self.reg.fit(X.toarray(), y.toarray())
             w = np.insert(self.reg.coef_, i, 0)[:,  np.newaxis]
             sim_mat.append(w)
     
-            #if i > 9:
+            #if i > 100:
             #    break
 
         self.sim_mat = np.concatenate(sim_mat, axis=1)
-    
+
+    def fit_multi(self, user_item_train_df):
+        # rating_mat
+        self.row = np.array([r[0] for r in user_item_train_df.values], dtype=int)
+        self.col = np.array([r[1] for r in user_item_train_df.values], dtype=int)
+        self.data = np.ones(len(user_item_train_df), dtype=int)
+        self.rating_mat = csr_matrix((self.data, (self.row, self.col)), shape = (self.user_num, self.item_num))
         
+        # linear modelを解く
+        sim_mat = Parallel(n_jobs=-1)([delayed(self.solve_lin_model)(n) for n in range(self.item_num)])
+        self.sim_mat = np.concatenate(sim_mat, axis=1)
+        
+    def solve_lin_model(self, idx):
+        X = self.del_col(idx)
+        y = self.rating_mat[:, idx]
+        self.reg.fit(X.toarray(), y.toarray())
+        w = np.insert(self.reg.coef_, idx, 0)[:,  np.newaxis]
+        return w
+
+        
+
     def del_col(self, col_idx):
         row_new = self.row[self.col != col_idx]
         col_new = self.col[self.col != col_idx]
