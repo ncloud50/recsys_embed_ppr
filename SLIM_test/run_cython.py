d1534d480b13224a9dce376b9963f4ec92b0729b
szktkfm <szk.tkfm@frontier.hokudai.ac.jp>
2020-08-19 12:57:44 +0000

    fix

--- SLIM_test/run_cython.py
+++
@@ -1,8 +0,0 @@
-import preprocess_cython
-import time
-
-if __name__ == '__main__':
-    s = time.time()
-    preprocess_cython.func()
-    runtime = time.time() - s
-    print(runtime)
--- SLIM_test/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    name='Hello world app',
-    ext_modules=cythonize("preprocess_cython.pyx"),
-    zip_safe=False,
-)
