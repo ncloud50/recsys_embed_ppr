4d6d395c6eb8f0455cc99038609b38d552da2e36
szktkfm <szk.tkfm@frontier.hokudai.ac.jp>
2020-09-02 15:56:46 +0000

    fix

--- KG_Embed/run.py
+++ KG_Embed/run.py
@@ -58,7 +58,7 @@ def objective(trial):
         iterater = TrainIterater(batch_size=int(batch_size), data_dir=dir_path, model_name=model_name)
         score =iterater.iterate_epoch(model, lr=lr, epoch=3000, weight_decay=weight_decay, warmup=warmup,
                             lr_decay_rate=lr_decay_rate, lr_decay_every=lr_decay_every, eval_every=1e+5, 
-                            early_stop=False)
+                            early_stop=True)
 
         score_sum += score 
     
