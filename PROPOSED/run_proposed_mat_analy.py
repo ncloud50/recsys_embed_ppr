6f4be0226111e13b762425dc9f299c745bb3fae3
szktkfm <szk.tkfm@frontier.hokudai.ac.jp>
2020-09-19 10:06:00 +0000

    fix a bit

--- PROPOSED/run_proposed_mat_analy.py
+++ PROPOSED/run_proposed_mat_analy.py
@@ -29,7 +29,6 @@ warnings.filterwarnings('ignore')
 
 
 
-
 def train_embed(data_dir, params, model_name):
     # ハイパラ読み込み
     embedding_dim = params['embedding_dim']
@@ -101,82 +100,6 @@ def mk_sparse_sim_mat(model, dataset, gamma):
     return M
 
 
-
-def pagerank_scipy(G, sim_mat,  personal_vec=None, alpha=0.85, beta=0.01,
-                   max_iter=500, tol=1.0e-6, weight='weight',
-                   dangling=None):
-    
-    N = len(G)
-    if N == 0:
-        return {}
-
-    nodelist = G.nodes()
-    M = nx.to_scipy_sparse_matrix(G, nodelist=nodelist, weight=weight,
-                                  dtype=float)
-    S = scipy.array(M.sum(axis=1)).flatten()
-    S[S != 0] = 1.0 / S[S != 0]
-    Q = scipy.sparse.spdiags(S.T, 0, *M.shape, format='csr')
-    M = Q * M
-
-    # 遷移行列とsim_matを統合
-    #sim_mat = mk_sparse_sim_mat(G, item_mat)
-    M = beta * M + (1 - beta) * sim_mat
-    
-    # initial vector
-    x = scipy.repeat(1.0 / N, N)
-
-    
-    # Personalization vector
-    p = personal_vec
- 
-    dangling_weights = p
-    is_dangling = scipy.where(S == 0)[0]
-
-
-    #print(x.shape)
-    #print(M.shape)
-    #print(p.shape)
-    
-    ppr_mat = []
-    for i in range(p.shape[1]):
-        ppr = power_iterate(N, M, x, p[:, i], dangling_weights[:, i], is_dangling, 
-                            alpha, max_iter, tol)
-        ppr_mat.append(ppr)
-        
-        #if i > 100:
-        #    print(np.array(ppr_mat).shape)
-        #    break 
-        
-    return np.array(ppr_mat)
-    
-
-def power_iterate(N, M, x, p, dangling_weights, is_dangling, alpha, max_iter=500, tol=1.0e-6):
-    #print(M.shape)
-    #print(x.shape)
-    #print(p.shape)
-    # power iteration: make up to max_iter iterations
-    for i in range(max_iter):
-        xlast = x
-        x = alpha * (x * M + sum(x[is_dangling]) * dangling_weights) + \
-            (1 - alpha) * p
-        # check convergence, l1 norm
-        x = x / x.sum()
-        err = scipy.absolute(x - xlast).sum()
-        if err < N * tol:
-            #return dict(zip(nodelist, map(float, x)))
-            #print(i)
-            return x
-    # pagerankの収束ちゃんとやっとく
-    print(x.sum())
-    print(err)
-    print(N * tol)
-    #raise NetworkXError('pagerank_scipy: power iteration failed to converge '
-                        #'in %d iterations.' % max_iter)
-        
-    #return dict(zip(nodelist, map(float, x)))
-    return x
-
-
 def pagerank_fast(G, sim_mat, personal_vec, alpha, beta):
     nodelist = G.nodes()
     M = nx.to_scipy_sparse_matrix(G, nodelist=nodelist, weight='weight',
@@ -234,90 +157,20 @@ def get_ranking_mat(G, dataset, model, gamma, alpha=0.85, beta=0.01):
     return ranking_mat
 
 
-def topn_precision(ranking_mat, user_items_dict, n=10):
-    not_count = 0
-    precision_sum = 0
-    user_idx = [dataset.entity_list.index(u) for u in dataset.user_list]
-        
-    for i in range(len(ranking_mat)):
-        if len(user_items_dict[user_idx[i]]) == 0:
-            not_count += 1
-            continue
-        sorted_idx = ranking_mat[i]
-        topn_idx = sorted_idx[:n]  
-        hit = len(set(topn_idx) & set(user_items_dict[user_idx[i]]))
-        #precision = hit / len(user_items_dict[user_idx[i]])
-        precision = hit / n
-        precision_sum += precision
-        
-    return precision_sum / (len(user_idx) - not_count)
-
-
 def time_since(runtime):
     mi = int(runtime / 60)
     sec = int(runtime - mi * 60)
     return (mi, sec)
 
 
-def objective(trial):
-    start = time.time()
-    # hyper parameter
-    alpha = trial.suggest_uniform('alpha', 0, 0.5)
-    beta = trial.suggest_uniform('beta', 0, 0.5)
-    gamma1 = trial.suggest_uniform('gamma1', 0, 1)
-    gamma2 = trial.suggest_uniform('gamma2', 0, 1)
-    gamma3 = trial.suggest_uniform('gamma3', 0, 1)
-    gamma = [gamma1, gamma2, gamma3]
-    
-    data_dir = ['../' + data_path + '/valid1', '../' + data_path + '/valid2']
-    score_sum = 0
-    for i in range(len(data_dir)):
-        # dataload
-        dataset = AmazonDataset(data_dir[i], model_name='TransE')
-
-        # load network
-        edges = [[r[0], r[1]] for r in dataset.triplet_df.values]
-        # user-itemとitem-userどちらの辺も追加
-        for r in dataset.triplet_df.values:
-            if r[2] == 0:
-                edges.append([r[1], r[0]])
-
-        G = nx.DiGraph()
-        G.add_nodes_from([i for i in range(len(dataset.entity_list))])
-        G.add_edges_from(edges)
-
-        ranking_mat = get_ranking_mat(G, dataset, model[i], gamma, alpha, beta)
-        evaluater = Evaluater(data_dir[i])
-        score = evaluater.topn_map(ranking_mat)
-        score_sum += score
-
-    mi, sec = time_since(time.time() - start)
-    print('{}m{}sec'.format(mi, sec))
-    
-    return -1 * score_sum / 2
-
 
 if __name__ == '__main__':
-    args = sys.argv
-    amazon_data = args[1]
-    save_path = 'result_' + amazon_data
-    if amazon_data[0] == 'b':
-        data_path = 'data_' + amazon_data + '_2core_es'
-    elif amazon_data[0] == 'l':
-        data_path = 'data_' + amazon_data + '_5core'
+    M = pickle.load(open('model_sim.mat', 'rb'))
+    print(M.shape)
 
-    # kg_embedハイパラ
-    kgembed_param = pickle.load(open('./kgembed_params/best_param_TransE.pickle', 'rb'))
-    start = time.time()
-    model1 = train_embed('../' + data_path + '/valid1', kgembed_param, 'TransE')
-    model2 = train_embed('../' + data_path + '/valid2', kgembed_param, 'TransE')
-    model = [model1, model2]
-    mi, sec = time_since(time.time() - start)
-    print(mi, sec)
+    #print(M[M != 0].shape)
 
-    study = optuna.create_study()
-    study.optimize(objective, n_trials=50)
-    df = study.trials_dataframe() # pandasのDataFrame形式
-    df.to_csv(save_path + '/hyparams_result_TransE.csv')
-    with open(save_path + '/best_param_TransE.pickle', 'wb') as f:
-        pickle.dump(study.best_params, f)
+    # 値の分布を調べる
+    print(len(M.data))
+
+
